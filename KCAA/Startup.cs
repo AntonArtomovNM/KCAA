f25eecce8fd596305eaf809b4abcf283350c71b5
Anton Artomov <86731073+AntonArtomovNM@users.noreply.github.com>
2021-10-17 16:30:59 +0300

    feat: Added bot polling

--- KCAA/Startup.cs
+++ KCAA/Startup.cs
@@ -1,24 +1,44 @@
+using System.Threading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using KCAA.Services;
+using KCAA.Settings;
+using KCAA.Services.Interfaces;
 
 namespace KCAA
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly Container _container = new Container();
+        private IConfiguration _configuration;
+        private TelegramSettings _telegramSettings;
+
+        public Startup(IConfiguration configuration)
         {
+            _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _telegramSettings = _configuration.GetSection(TelegramSettings.ConfigKey).Get<TelegramSettings>();
+
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddSimpleInjector(_container, options =>
+            {
+                options.AddAspNetCore().AddControllerActivation();
+            });
+
+            InitializeContainer();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,6 +55,35 @@ namespace KCAA
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            _container.Verify();
+
+            InitializePolling();
+        }
+
+        private void InitializeContainer()
+        {
+            var botClient = GetBotClient();
+            _container.RegisterInstance(botClient);
+
+            _container.Register<ITelegramUpdateHandler, TelegramUpdateHandler>();
+        }
+
+        private ITelegramBotClient GetBotClient()
+        {
+            ITelegramBotClient botClient = new TelegramBotClient(_telegramSettings.BotToken);
+
+            botClient.SetMyCommandsAsync(_telegramSettings.BotCommands).GetAwaiter().GetResult();
+            return botClient;
+        }
+
+        private void InitializePolling()
+        {
+            var botClient = _container.GetInstance<ITelegramBotClient>();
+            var handler = _container.GetInstance<ITelegramUpdateHandler>();
+            var cts = new CancellationTokenSource();
+
+            botClient.StartReceiving(new DefaultUpdateHandler(handler.HandleUpdateAsync, handler.HandleErrorAsync), cts.Token);
         }
     }
 }
