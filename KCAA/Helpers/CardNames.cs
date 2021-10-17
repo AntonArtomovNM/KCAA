ff643a690a2525f870a7b1df497c26449a7cd65c
dammit.kseniya@gmail.com <dammit.kseniya@gmail.com>
2021-10-17 17:05:21 +0300

    Added constants for the names of cards and characters

--- KCAA/Helpers/CardNames.cs
+++ KCAA/Helpers/CardNames.cs
@@ -3,5 +3,77 @@
     public static class CardNames
     {
         public const string Green1 = "g1";
+
+        public const string Green2 = "g2";
+
+        public const string Green3 = "g3";
+
+        public const string Green4 = "g4";
+
+        public const string Green5 = "g5";
+        
+        public const string Blue1 = "b1";
+
+        public const string Blue2 = "b2";
+
+        public const string Blue3 = "b3";
+
+        public const string Blue4 = "b4";
+
+        public const string Blue5 = "b5";
+        
+        public const string Red1 = "r1";
+
+        public const string Red2 = "r2";
+
+        public const string Red3 = "r3";
+
+        public const string Red4 = "r4";
+
+        public const string Red5 = "r5";
+        
+        public const string Yellow1 = "y1";
+
+        public const string Yellow2 = "y2";
+
+        public const string Yellow3 = "y3";
+
+        public const string Yellow4 = "y4";
+
+        public const string Yellow5 = "y5";
+
+        public const string SecretHideout = "sp-secret-hideout";
+
+        public const string GhostNeighbourhood = "sp-ghost-neighbourhood";
+
+        public const string Stable = "sp-stable";
+
+        public const string Fort = "sp-fort";
+
+        public const string Goldmine = "sp-goldmine";
+
+        public const string DragonGates = "sp-dragon-gates";
+
+        public const string ImperialTreasury = "sp-imperial-treasury";
+
+        public const string Capitol = "sp-capitol";
+
+        public const string MapRoom = "sp-map-room";
+
+        public const string WishingWell = "sp-wishing-well";
+
+        public const string Forge = "sp-forge";
+
+        public const string Laboratory = "sp-laboratory";
+
+        public const string Museum = "sp-museum";
+
+        public const string Armory = "sp-armory";
+
+        public const string Scaffolding = "sp-scaffolding";
+
+        public const string Monument = "sp-monument";
+
+        public const string MagicSchool = "sp-magic-school";
     }
 }
