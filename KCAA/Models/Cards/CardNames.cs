16c24c7fbb80cd86de680d70619e011bb181c661
dammit.kseniya@gmail.com <dammit.kseniya@gmail.com>
2021-11-21 16:45:08 +0200

    Corrected character description and replace const strings with static properties

--- KCAA/Models/Cards/CardNames.cs
+++ KCAA/Models/Cards/CardNames.cs
@@ -2,78 +2,78 @@
 {
     public static class CardNames
     {
-        public const string Green1 = "g1";
+        public static string Green1 => "g1";
 
-        public const string Green2 = "g2";
+        public static string Green2 => "g2";
 
-        public const string Green3 = "g3";
+        public static string Green3 => "g3";
 
-        public const string Green4 = "g4";
+        public static string Green4 => "g4";
 
-        public const string Green5 = "g5";
+        public static string Green5 => "g5";
 
-        public const string Blue1 = "b1";
+        public static string Blue1 => "b1";
 
-        public const string Blue2 = "b2";
+        public static string Blue2 => "b2";
 
-        public const string Blue3 = "b3";
+        public static string Blue3 => "b3";
 
-        public const string Blue4 = "b4";
+        public static string Blue4 => "b4";
 
-        public const string Blue5 = "b5";
+        public static string Blue5 => "b5";
 
-        public const string Red1 = "r1";
+        public static string Red1 => "r1";
 
-        public const string Red2 = "r2";
+        public static string Red2 => "r2";
 
-        public const string Red3 = "r3";
+        public static string Red3 => "r3";
 
-        public const string Red4 = "r4";
+        public static string Red4 => "r4";
 
-        public const string Red5 = "r5";
+        public static string Red5 => "r5";
 
-        public const string Yellow1 = "y1";
+        public static string Yellow1 => "y1";
 
-        public const string Yellow2 = "y2";
+        public static string Yellow2 => "y2";
 
-        public const string Yellow3 = "y3";
+        public static string Yellow3 => "y3";
 
-        public const string Yellow4 = "y4";
+        public static string Yellow4 => "y4";
 
-        public const string Yellow5 = "y5";
+        public static string Yellow5 => "y5";
 
-        public const string SecretHideout = "sp-secret-hideout";
+        public static string SecretHideout => "sp-secret-hideout";
 
-        public const string GhostNeighbourhood = "sp-ghost-neighbourhood";
+        public static string GhostNeighbourhood => "sp-ghost-neighbourhood";
 
-        public const string Stable = "sp-stable";
+        public static string Stable => "sp-stable";
 
-        public const string Fort = "sp-fort";
+        public static string Fort => "sp-fort";
 
-        public const string Goldmine = "sp-goldmine";
+        public static string Goldmine => "sp-goldmine";
 
-        public const string DragonGates = "sp-dragon-gates";
+        public static string DragonGates => "sp-dragon-gates";
 
-        public const string ImperialTreasury = "sp-imperial-treasury";
+        public static string ImperialTreasury => "sp-imperial-treasury";
 
-        public const string Capitol = "sp-capitol";
+        public static string Capitol => "sp-capitol";
 
-        public const string MapRoom = "sp-map-room";
+        public static string MapRoom => "sp-map-room";
 
-        public const string WishingWell = "sp-wishing-well";
+        public static string WishingWell => "sp-wishing-well";
 
-        public const string Forge = "sp-forge";
+        public static string Forge => "sp-forge";
 
-        public const string Laboratory = "sp-laboratory";
+        public static string Laboratory => "sp-laboratory";
 
-        public const string Museum = "sp-museum";
+        public static string Museum => "sp-museum";
 
-        public const string Armory = "sp-armory";
+        public static string Armory => "sp-armory";
 
-        public const string Scaffolding = "sp-scaffolding";
+        public static string Scaffolding => "sp-scaffolding";
 
-        public const string Monument = "sp-monument";
+        public static string Monument => "sp-monument";
 
-        public const string SchoolOfMagic = "sp-school-of-magic";
+        public static string SchoolOfMagic => "sp-school-of-magic";
     }
 }
